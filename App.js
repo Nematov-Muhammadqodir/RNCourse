6be1a491c4b3ca134868f5ffdacce92b392177a0
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-09-12 14:42:10 +0900

    feat: change ScrollView into FlatList component

--- App.js
+++ App.js
@@ -1,19 +1,84 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [goal, setGoal] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const handleListOfGoals = () => {
+    setGoals((goals) => [
+      ...goals,
+      { text: goal, id: Math.random().toString() },
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Hello Worl!</Text>
-      <Button title="Tap me" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goals!"
+          value={goal}
+          onChangeText={(text) => setGoal(text)}
+        />
+        <Button title="Add Goal" onPress={handleListOfGoals} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={goals}
+          renderItem={(itemData) => (
+            <View style={styles.listItem}>
+              <Text>{itemData.item.text}</Text>
+            </View>
+          )}
+          alwaysBounceVertical={false}
+          keyExtractor={(item, key) => {
+            return item.id;
+          }}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: "white",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    paddingBottom: 16,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  listItem: {
+    padding: 8,
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 2,
+    borderColor: "green",
   },
 });
