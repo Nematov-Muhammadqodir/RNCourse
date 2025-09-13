403b8936ced24145b22eb24ab4d31ae370b27890
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-09-13 18:31:03 +0900

    feat: use Image component

--- App.js
+++ App.js
@@ -58,6 +58,7 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 16,
     flex: 1,
+    position: "relative",
   },
   goalsContainer: {
     flex: 5,
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,4 +1,13 @@
-import { StyleSheet, View, TextInput, Button, Modal } from "react-native";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Modal,
+  Image,
+  Pressable,
+  Text,
+} from "react-native";
 import { useState } from "react";
 
 const GoalInput = (props) => {
@@ -13,19 +22,28 @@ const GoalInput = (props) => {
   };
   return (
     <Modal visible={isModalVisible} animationType="slide">
+      <Image
+        source={require("../assets/images/target.jpg")}
+        style={styles.image}
+      />
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
           placeholder="Your course goals!"
           value={goal}
           onChangeText={(text) => setGoal(text)}
+          placeholderTextColor="white"
         />
         <View style={styles.buttonsContainer}>
           <View style={styles.button}>
-            <Button title="Add Goal" onPress={handleListOfGoals} />
+            <Pressable onPress={handleListOfGoals}>
+              <Text style={styles.text}>Add Goal</Text>
+            </Pressable>
           </View>
-          <View style={styles.button}>
-            <Button title="Cancel" onPress={() => setIsModelVisible(false)} />
+          <View style={styles.cancel}>
+            <Pressable onPress={() => setIsModelVisible(false)}>
+              <Text style={styles.text}>Cancel</Text>
+            </Pressable>
           </View>
         </View>
       </View>
@@ -40,11 +58,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 24,
-    marginTop: 24,
     borderBottomWidth: 1,
     borderBottomColor: "#ccc",
     paddingBottom: 16,
+    backgroundColor: "rgba(0,0,0,0.9)",
   },
   textInput: {
     borderWidth: 1,
@@ -52,6 +69,7 @@ const styles = StyleSheet.create({
     width: "70%",
     marginRight: 8,
     padding: 8,
+    color: "white",
   },
   buttonsContainer: {
     marginTop: 16,
@@ -61,5 +79,37 @@ const styles = StyleSheet.create({
   button: {
     marginHorizontal: 8,
     width: "40%",
+    borderWidth: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 6,
+    borderColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "green",
+  },
+  cancel: {
+    marginHorizontal: 8,
+    width: "40%",
+    borderWidth: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 6,
+    borderColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "red",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    zIndex: -1,
+    position: "absolute",
+    transform: [{ scale: 1.2 }],
+  },
+  text: {
+    color: "white",
+    letterSpacing: 1,
+    fontWeight: "bold",
   },
 });
